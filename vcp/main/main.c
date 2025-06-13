5594a05c05a8b26bc15f20a1882d8f6bdbfa96d4
nopnop2002 <>
2025-06-13 09:15:28 +0900

    added vcp project

---
+++ vcp/main/main.c
@@ -0,0 +1,190 @@
+/* The example of CC1101
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "esp_log.h"
+
+#include <cc1101.h>
+
+static const char *TAG = "MAIN";
+
+MessageBufferHandle_t xMessageBufferTx;
+MessageBufferHandle_t xMessageBufferRx;
+
+// The total number of bytes (not messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 65; // Maximum Payload size of CC1101 is 64
+
+#if CONFIG_SENDER
+void tx_task(void *pvParameter)
+{
+	ESP_LOGI(pcTaskGetName(0), "Start");
+	CCPACKET packet;
+	uint8_t buf[xItemSize];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRx, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, received, ESP_LOG_DEBUG);
+
+		// The VCP termination character is CR+LF. So Remove CR/LF.
+		packet.length = 0;
+		for (int i=0;i<received;i++) {
+			if (buf[i] == 0x0d) continue;
+			if (buf[i] == 0x0a) continue;
+			packet.data[packet.length] = buf[i];
+			packet.length++;
+		}
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), packet.data, packet.length, ESP_LOG_INFO);
+		sendData(packet);
+		ESP_LOGI(pcTaskGetName(0), "Sent packet. length=%d", packet.length);
+	} // end while
+
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+// Get signal strength indicator in dBm.
+// See: http://www.ti.com/lit/an/swra114d/swra114d.pdf
+int rssi(char raw) {
+	uint8_t rssi_dec;
+	// TODO: This rssi_offset is dependent on baud and MHz; this is for 38.4kbps and 433 MHz.
+	uint8_t rssi_offset = 74;
+	rssi_dec = (uint8_t) raw;
+	if (rssi_dec >= 128)
+		return ((int)( rssi_dec - 256) / 2) - rssi_offset;
+	else
+		return (rssi_dec / 2) - rssi_offset;
+}
+
+// Get link quality indicator.
+int lqi(char raw) {
+	return 0x3F - raw;
+}
+
+void rx_task(void *pvParameter)
+{
+	ESP_LOGI(pcTaskGetName(0), "Start");
+	CCPACKET packet;
+	uint8_t buf[xItemSize];
+	while(1) {
+		if(packet_available()) {
+			if (receiveData(&packet) > 0) {
+				ESP_LOGI(pcTaskGetName(0), "Received packet...");
+				if (!packet.crc_ok) {
+					ESP_LOGE(pcTaskGetName(0), "crc not ok");
+				} else {
+					ESP_LOGI(pcTaskGetName(0),"packet.lqi: %d", lqi(packet.lqi));
+					ESP_LOGI(pcTaskGetName(0),"packet.rssi: %ddBm", rssi(packet.rssi));
+					ESP_LOGI(pcTaskGetName(0),"packet.length: %d", packet.length);
+					if (packet.length > 0) {
+						ESP_LOGI(pcTaskGetName(0),"data: %.*s", packet.length, (char *)packet.data);
+						ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), packet.data, packet.length, ESP_LOG_INFO);
+
+						memcpy(buf, packet.data, packet.length);
+						int txLen = packet.length;
+						if (buf[txLen-1] != 0x0a) {
+							buf[txLen] = 0x0a;
+							txLen++;
+						}
+						size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTx );
+						ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+						size_t sended = xMessageBufferSend(xMessageBufferTx, buf, txLen, 100);
+						if (sended != txLen) {
+							ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail txLen=%d sended=%d", txLen, sended);
+							break;
+						}
+					}
+					
+				}
+			} // end receiveData
+		} // end packet_available
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_RECEIVER
+
+void cdc_acm_vcp_task(void *pvParameters);
+
+void app_main()
+{
+	// Create MessageBuffer
+	xMessageBufferTx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTx );
+	xMessageBufferRx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRx );
+
+	uint8_t freq;
+#if CONFIG_CC1101_FREQ_315
+	freq = CFREQ_315;
+	ESP_LOGW(TAG, "Set frequency to 315MHz");
+#elif CONFIG_CC1101_FREQ_433
+	freq = CFREQ_433;
+	ESP_LOGW(TAG, "Set frequency to 433MHz");
+#elif CONFIG_CC1101_FREQ_868
+	freq = CFREQ_868;
+	ESP_LOGW(TAG, "Set frequency to 868MHz");
+#elif CONFIG_CC1101_FREQ_915
+	freq = CFREQ_915;
+	ESP_LOGW(TAG, "Set frequency to 915MHz");
+#endif
+
+	uint8_t mode;
+#if CONFIG_CC1101_SPEED_4800
+	mode = CSPEED_4800;
+	ESP_LOGW(TAG, "Set speed to 4800bps");
+#elif CONFIG_CC1101_SPEED_9600
+	mode = CSPEED_9600;
+	ESP_LOGW(TAG, "Set speed to 38400bps");
+#elif CONFIG_CC1101_SPEED_19200
+	mode = CSPEED_19200;
+	ESP_LOGW(TAG, "Set speed to 38400bps");
+#elif CONFIG_CC1101_SPEED_38400
+	mode = CSPEED_38400;
+	ESP_LOGW(TAG, "Set speed to 38400bps");
+#endif
+
+	//init(CFREQ_433, 0);
+	esp_err_t ret = init(freq, mode);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "CC1101 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+	uint8_t syncWord[2] = {199, 10};
+	setSyncWordArray(syncWord);
+	ESP_LOGW(TAG, "Set channel to %d", CONFIG_CC1101_CHANNEL);
+	setChannel(CONFIG_CC1101_CHANNEL);
+	disableAddressCheck();
+#if CONFIG_CC1101_POWER_MIN
+	ESP_LOGW(TAG, "Set Minimum power level");
+	setTxPowerAmp(POWER_MIN);
+#elif CONFIG_CC1101_POWER_0db
+	ESP_LOGW(TAG, "Set 0 dBm power level");
+	setTxPowerAmp(POWER_0db);
+#elif CONFIG_CC1101_POWER_MAX
+	ESP_LOGW(TAG, "Set Maximum power level");
+	setTxPowerAmp(POWER_MAX);
+#endif
+
+#if CONFIG_SENDER
+	xTaskCreate(&tx_task, "TX", 1024*3, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&rx_task, "RX", 1024*3, NULL, 5, NULL);
+#endif
+	// Start CDC_ACM_VCP
+	xTaskCreate(cdc_acm_vcp_task, "CDC_ACM_VCP", 1024*4, NULL, 5, NULL);
+}
+
