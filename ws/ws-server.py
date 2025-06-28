061fb92bb510cff3c0db1dcd4694b0406ef7926f
nopnop2002 <>
2025-06-29 08:29:11 +0900

    Changed the priority of esp_websocket_client_task

--- ws/ws-server.py
+++ ws/ws-server.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
-# https://github.com/Pithikos/python-websocket-server
 # python3 -m pip install websocket-server
+# https://github.com/Pithikos/python-websocket-server
 
 import argparse
-import logging
 from websocket_server import WebsocketServer
 
+# Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("new client connected and was given id {}".format(client['id']))
 
+# Called for every client disconnecting
 def client_left(client, server):
 	print("client({}) disconnected".format(client['id']))
 
+# Called when a client sends a message
 def message_received(client, server, message):
 	print("client({}) said: {}".format(client['id'], message))
 	responce = "ok"
-	# server.send_message_to_all(responce)
 	server.send_message(client, responce)
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--port', type=int, help='socket port', default=8080)
+	parser.add_argument('--port', type=int, help='listen port', default=8080)
 	args = parser.parse_args()
 	print("args.port={}".format(args.port))
 
-	server = WebsocketServer(host='0.0.0.0', port=args.port, loglevel=logging.INFO)
+	server = WebsocketServer(host="0.0.0.0", port=args.port)
 	server.set_fn_new_client(new_client)
 	server.set_fn_client_left(client_left)
 	server.set_fn_message_received(message_received)
